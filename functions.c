2c3e59fa209768f988975298d1525a9d874410f4
Luke Tuthill <lukemtuthill@gmail.com>
2018-10-04 12:59:45 +1000

    View by tags

--- functions.c
+++ functions.c
@@ -8,7 +8,7 @@ void pinMode(int pin, int mode);
 
 int digitalRead(int pin);
 // :param pin: The pin to read from
-// :tags: pins input
+// :tags: pins
 // :week: 3
 // :returns: The value of the pin being read
 // Reads a digital signal (HIGH or LOW) from a pin.
@@ -16,21 +16,21 @@ int digitalRead(int pin);
 void digitalWrite(int pin, int value);
 // :param pin: The pin to write to
 // :param value: The value to be written (HIGH or LOW)
-// :tags: pins output
+// :tags: pins
 // :week: 3
 // Writes a digital signal (HIGH or LOW) to a pin.
 
 int analogRead(int pin);
 // :param pin: The pin to read from (A0-A5)
 // :returns: The analog value of the pin: 0 to 1023
-// :tags: pins input analog
+// :tags: pins
 // :week: 3
 // Reads an analog signal from a pin.
 
 void analogWrite(int pin, int value);
 // :param pin: The pin to write to (A0-A5)
 // :param value: The value to write (0-1023)
-// :tags: pins output analog
+// :tags: pins
 // :week: 3
 // Writes an analog value to a pin.
 
@@ -43,13 +43,13 @@ void Serial.begin(int baudrate);
 
 void Serial.print(any item);
 // :param item: The data you want to print
-// :tags: serial debug
+// :tags: serial
 // :week: 4
 // Prints data to the computer over serial, without the trailing newline.
 
 void Serial.println(any item);
 // :param item: The data you want to print
-// :tags: serial debug
+// :tags: serial
 // :week: 4
 // Prints data to the computer over serial, including the trailing newline.
 
@@ -57,25 +57,25 @@ void tone(int pin, float frequency, float duration);
 // :param pin: The pin that the buzzer is connected to
 // :param frequency: The frequency to play
 // :param duration opt: How long to play for
-// :tags: output pwm audio
+// :tags: audio
 // :week: 5
 // Plays a frequency on a particular pin.
 // You can optionally specify a duration, and the tone will play and stop after that duration while your code continues to run. 
 
 void noTone(int pin);
 // :param pin: The pin to silence
-// :tags: output pwm audio
+// :tags: audio
 // :week: 5
 // Immediately stops all tones on a pin.
 
 void CurieIMU.begin();
-// :tags: curie imu setup
+// :tags: imu setup
 // :week: 5
 // Initialises the Curie IMU library.
 
 void CurieIMU.setAccelerometerRange(int range)
 // :param range: The range of the accelerometer: 2, 4, 8 or 16
-// :tags: curie imu setup
+// :tags: imu setup
 // :week: 5
 // Sets the range of the accelerometer - the minimum and maximum values that it can record.
 // Higher values mean less precision, but a greater maximum acceleration that can be measured.
@@ -85,7 +85,7 @@ void CurieIMU.readAccelerometer(int x, int y, int z);
 // :param x: A variable that the function will fill with the X acceleration value
 // :param y: A variable that the function will fill with the Y acceleration value
 // :param z: A variable that the function will fill with the Z acceleration value
-// :tags: curie imu input
+// :tags: imu
 // :week: 5
 // Reads x, y and z acceleration values into the variables provided.
 // Values will be between -32,768 and 32,768, and what these numbers represent depend on the accelerometer range set in CurieIMU.setAccelerometerRange().
@@ -95,6 +95,21 @@ void CurieIMU.readAccelerometerScaled(float x, float y, float z);
 // :param x: A variable that the function will fill with the X acceleration value
 // :param y: A variable that the function will fill with the Y acceleration value
 // :param z: A variable that the function will fill with the Z acceleration value
-// :tags: curie imu input
+// :tags: imu
 // :week: 5
 // Reads x, y and z acceleration values into the variables provided, scaling and converting them to G-force.
+
+void CurieIMU.attachInterrupt(function callback);
+// :param callback: The function to be called when an IMU event is detected
+// :tags: imu
+// :week: 5
+// Attach a callback function to an interrupt.
+// This function will be called whenever an event that you have selected using CurieIMU.interrupts() occurs.
+
+void CurieIMU.interrupts(int event);
+// :param event: The event to detect
+// :tags: imu
+// Select a type of event to detect.
+// When the event you specify has been detected, the callback function you passsed to CurieIMU.attachInterrupt will be called.
+// The event parameter can be one of the following:
+// CURIE_IMU_FREEFALL, CURIE_IMU_SHOCK, CURIE_IMU_MOTION, CURIE_IMU_ZERO_MOTION, CURIE_IMU_STEP, CURIE_IMU_TAP, CURIE_IMU_TAP_SHOCK, CURIE_IMU_TAP_QUIET, CURIE_IMU_DOUBLE_TAP
--- render.js
+++ render.js
@@ -1,168 +1,209 @@
 var reference = [];
+var lastSearch = '';
 
 function loadContent() {
-    return $.getJSON("reference.json")
-        .done(function (data) {
-            reference = data;
-        })
-        .fail(function (j, s, e) {
-            console.log(e)
-        }).promise();
+	return $.getJSON("reference.json")
+		.done(function (data) {
+			reference = data;
+		})
+		.fail(function (j, s, e) {
+			console.log(e)
+		}).promise();
 }
 
 function makeArg(name, type, desc, optional) {
-    var argClass = $('<span>')
-        .addClass('arg')
-        .addClass(type)
-        .text(optional ? '[' + name + ']' : name)
-        .attr('arg-desc', desc)
-        .attr('arg-type', type)
-        .hover(function() {
-            $(this).children().fadeIn(100);
-        }, function() {
-            $(this).children().fadeOut(100);
-        })
-    var argTooltip = $('<div>')
-        .addClass('arg-tooltip')
-        .append($('<span>').addClass('arg-tooltip-text')
-            .text(desc)
-        )
-        .hide()
-    argClass.append(argTooltip);
-    return argClass;
+	var argClass = $('<span>')
+		.addClass('arg')
+		.addClass('arg-type-' + type)
+		.text(optional ? '[' + name + ']' : name)
+		.attr('arg-desc', desc)
+		.attr('arg-type', type)
+		.hover(function() {
+			$(this).children().fadeIn(100);
+		}, function() {
+			$(this).children().fadeOut(100);
+		})
+	var argTooltip = $('<div>')
+		.addClass('arg-tooltip')
+		.append($('<span>').addClass('arg-tooltip-text')
+			.text(desc)
+		)
+		.hide()
+	argClass.append(argTooltip);
+	return argClass;
 }
 
 function makeTag(tag) {
-    var tagClass = $('<div>')
-        .addClass('tag')
-        .addClass('tag-' + tag)
-        .text(tag)
-        .hover(function() {
-            var current = $(this);
-            $('.tag-' + current.text()).not(current).parents('div.function').addClass('highlighted')
-        }, function() {
-            var current = $(this);
-            $('.tag-' + current.text()).not(current).parents('div.function').removeClass('highlighted')
-        });
-    return tagClass;
+	var tagClass = $('<div>')
+		.addClass('tag')
+		.addClass('tag-' + tag)
+		.text(tag)
+		.hover(function() {
+			var current = $(this);
+			$('.tag-' + current.text()).not(current).parents('div.function').addClass('highlighted')
+		}, function() {
+			var current = $(this);
+			$('.tag-' + current.text()).not(current).parents('div.function').removeClass('highlighted')
+		});
+	return tagClass;
 }
 
 function constructFunction(f) {
-    var div = $('<div>').addClass('function');
-    div.append($('<div>').addClass('function-name').text(f.function))
-    var info = $('<div>').addClass('function-info')
-    var usage = $('<div>').addClass('function-usage');
+	var div = $('<div>').addClass('function');
+	div.append($('<div>').addClass('function-name').text(f.function))
+	var info = $('<div>').addClass('function-info')
+	var usage = $('<div>').addClass('function-usage');
 
-    var args = [];
-    usage.append(f.function + '(');
-    for (var i = 0; i < f.arguments.length; i++) {
-        var arg = f.arguments[i];
-        usage.append(makeArg(arg.name, arg.type, arg.desc, arg.optional));
-        if (i < f.arguments.length - 1) {
-            usage.append(', '); 
-        }
-    }
-    usage.append(')');
-    if (f.returns) {
-        var returns = $('<span>').addClass('returns');
-        returns.append(' -> ')
-        returns.append(makeArg(f.returns.type, f.returns.type, f.returns.desc, false));
-    }
+	var args = [];
+	usage.append(f.function + '(');
+	for (var i = 0; i < f.arguments.length; i++) {
+		var arg = f.arguments[i];
+		usage.append(makeArg(arg.name, arg.type, arg.desc, arg.optional));
+		if (i < f.arguments.length - 1) {
+			usage.append(', '); 
+		}
+	}
+	usage.append(')');
+	if (f.returns) {
+		var returns = $('<span>').addClass('returns');
+		returns.append(' -> ')
+		returns.append(makeArg(f.returns.type, f.returns.type, f.returns.desc, false));
+	}
 
-    usage.append(returns);
+	usage.append(returns);
 
-    var desc = $('<div>').addClass('function-desc');
+	var desc = $('<div>').addClass('function-desc');
 
-    $.each(f.desc, function() {
-        switch (this.type) {
-            case 'text':
-                desc.append($('<p>').text(this.content));
-                break;
-        }
-    })
+	$.each(f.desc, function() {
+		switch (this.type) {
+			case 'text':
+				desc.append($('<p>').text(this.content));
+				break;
+		}
+	})
 
-    var tags = $('<div>').addClass('function-tags');
+	var tags = $('<div>').addClass('function-tags');
 
-    $.each(f.tags, function() {
-        tags.append(makeTag(this));
-    })
+	$.each(f.tags, function() {
+		tags.append(makeTag(this));
+	})
 
-    info.append($('<span>').addClass('function-usage-text').text("Usage: "));
-    info.append(usage);
-    info.append(desc);
-    info.append(tags);
-    info.toggle();
-    div.append(info);
-    div.click(function() {
-        $(this).children('.function-info').toggle(100);
-        $(this).children('.function-name').toggleClass('underline');
-    })
-    return div;
+	info.append($('<span>').addClass('function-usage-text').text("Usage:"));
+	info.append(usage);
+	info.append(desc);
+	info.append(tags);
+	info.toggle();
+	div.append(info);
+	div.click(function() {
+		$(this).children('.function-info').toggle(100);
+		$(this).children('.function-name').toggleClass('underline');
+	})
+	return div;
 }
 
 function viewByWeek() {
-    var weeks = [];
-    var has_general = false;
-    $.each(reference.functions, function() {
-        console.log("Found week " + this.week);
-        if (this.hasOwnProperty('week'))
-            weeks.push(this.week);
-        else has_general = true;
-    });
-    weeks.sort();
-    $.each(weeks, function() {
-        if ($('.content').children('#week-' + this).length == 0) {
-            console.log("Adding week " + this);
-            var week = $('<div>')
-                .addClass('week')
-                .attr("id", 'week-' + this)
-                .append(
-                    $('<div>')
-                    .addClass('week-heading')
-                    .text("Week " + this)
-                );
-            week.append($('<div>').addClass('function-list'));
-            $('.content').append(week);
-        }
-    });
-    if (has_general) {
-        var general = $('<div>')
-            .addClass('week')
-            .attr("id", 'week-general')
-            .append(
-                $('<div>')
-                .addClass('week-heading')
-                .text("General")
-            );
-        general.append($('<div>').addClass('function-list'));
-        $('.content').append(general);
-    }
-    $.each(reference.functions, function() {
-        console.log("Adding " + this.function)
-        if (this.hasOwnProperty("week"))
-            $('#week-' + this.week + '> .function-list').append(constructFunction(this));
-        else
-            $('#week-general > .function-list').append(constructFunction(this));
-    });
+	$('.content').empty();
+	var weeks = [];
+	var has_general = false;
+	$.each(reference.functions, function() {
+		if (this.hasOwnProperty('week'))
+			weeks.push(this.week);
+		else has_general = true;
+	});
+	weeks.sort();
+	$.each(weeks, function() {
+		if ($('.content').children('#week-' + this).length == 0) {
+			var week = $('<div>')
+				.addClass('week')
+				.attr("id", 'week-' + this)
+				.append(
+					$('<div>')
+					.addClass('week-heading')
+					.text("Week " + this)
+				);
+			week.append($('<div>').addClass('function-list'));
+			$('.content').append(week);
+		}
+	});
+	if (has_general) {
+		var general = $('<div>')
+			.addClass('week')
+			.attr("id", 'week-general')
+			.append(
+				$('<div>')
+				.addClass('week-heading')
+				.text("General")
+			);
+		general.append($('<div>').addClass('function-list'));
+		$('.content').append(general);
+	}
+	$.each(reference.functions, function() {
+		if (this.hasOwnProperty("week"))
+			$('#week-' + this.week + '> .function-list').append(constructFunction(this));
+		else
+			$('#week-general > .function-list').append(constructFunction(this));
+	});
+}
+
+function viewByTag() {
+	$('.content').empty();
+	var tags = {};
+	$.each(reference.functions, function(_, func) {
+		$.each(func.tags, function(_, tag) {
+			if (!tags.hasOwnProperty(tag)) {
+				tags[tag] = [];
+			}
+			tags[tag].push(func)
+		});
+	});
+
+	$.each(tags, function(key, value) {
+		var tagCont = $('<div>')
+			.addClass('tag-cont')
+			.attr('id', 'tag-cont-' + key)
+			.append(
+				$('<div>')
+				.addClass('tag-heading')
+				.text(key.toUpperCase())
+			)
+		var functionList = $('<div>').addClass('function-list');
+		$.each(value, function(_, func) {
+			functionList.append(constructFunction(func));
+		});
+		tagCont.append(functionList);
+		$('.content').append(tagCont)
+	})
 }
 
 function filterFunctions() {
-    var text = $('.search > input').val().toLowerCase();
-    console.log(text);
-    $('.function').filter(function() {
-        return !$(this).text().toLowerCase().includes(text)
-    }).hide()
-    $('.function').filter(function() {
-        return $(this).text().toLowerCase().includes(text)
-    }).show().parent().parent().show()
-    $(".function:hidden").parent().parent().not($(":visible").parent().parent()).hide();
+	var text = $('.search > input').val().toLowerCase().trim();
+	if (text == lastSearch) return;
+	lastSearch = text;
+	if (text == '') {
+		$('.function').show().parent().parent().show();
+		return;
+	}
+	$('.function').filter(function() {
+		return !$(this).text().toLowerCase().includes(text)
+	}).hide().parent().parent().not($(".function:visible").parent().parent()).hide();
+
+
+	$('.function').filter(function() {
+		return $(this).text().toLowerCase().includes(text)
+	}).show().parent().parent().show()
 }
 
+$(document).keyup(function(event) {
+	if (event.which == 70 || event.which == 191)
+		$('.search > input').focus();
+});
+
 $(document).ready(function() {
-    $('.search > input').keyup(filterFunctions);
-    loadContent()
-        .done(function() {
-            console.log("Done loading. Rendering now.");
-            viewByWeek();
-        });
+	$('.search > input').keyup(filterFunctions);
+	loadContent()
+		.done(function() {
+			console.log("Done loading. Rendering now.");
+			viewByTag(); 
+			// viewByWeek();
+		});
 });
